d5d42ab41393e7df7eebf46a4bd93cecfa348efe
deviindriawati <deviindriawati02@gmail.com>
2021-02-11 17:16:13 -0800

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Devi indriawati
+203040039
+https://github.com/Deviindriawati/pw2021_203040039
+pertemuan 2 - 8 februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+<?php
+// pertemuan 2 - pw2021_203040039
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump 
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML 
+// 2. HTML di dalam PHP 
+
+// Variabel dan Tipe Data 
+// Variabel 
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka 
+// $nama = "Devi indriawati";
+// echo "halo, nama saya $nama";
+
+// Operator 
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// . 
+// $nama_depan = "Devi";
+// $nama_belakang = "Indriawati";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment 
+// =, +=, -=, *=, /=, %=, .= 
+// $x = 1;
+// $x -= 5;
+// echo $x; 
+// $nama = "Devi";
+// $nama .= " ";
+// $nama .= "Indriawati";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas 
+// ===, !==
+// var_dump(1 === "1")
+
+// Logika
+// &&, ||, ! 
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0 );
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>halo, Selamat Datang <?php echo "Devi Indriawati" ?></h1>
+</body>
+</html>
+
